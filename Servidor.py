278a308ae89c4ba17c08a578137b478984138666
JuanCarlosMiranda02 <jc.miranda19@info.uas.edu.mx>
2023-11-17 22:47:02 -0700

    Codigos correctos xd

---
+++ Servidor.py
@@ -0,0 +1,45 @@
+import socket
+import threading
+global condicion
+# Función que maneja la conexión para un cliente
+def handle_client(client_socket):
+    try:
+        client_address = client_socket.getpeername()
+        data = client_socket.recv(1024)
+        print('Conexión de: ', client_address)
+        print('Se recibieron datos de {}: {}'.format(client_address, data.decode()))
+        message = b'Usted se ha conectado exitosamente al servidor'
+        client_socket.send(message)
+        condicion=True
+        while condicion:
+            data = client_socket.recv(1024)
+            if not data:
+                print('No hay datos de', client_address)
+                break
+            command_parts = data.decode().strip().split(maxsplit=1)
+            comando = command_parts[0]
+            if "echo" in comando:
+                if len(command_parts) > 1:
+                    response_message = bytes(command_parts[1], 'utf-8') 
+                    client_socket.send(response_message)
+                elif comando.lower()=='bye':
+                    condicion=False
+                    break
+    finally:
+        # Clean up the connection
+        client_socket.close()
+        print('Conexión cerrada con: ', client_address)
+# Create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Bind the socket to the port
+server_address = ('0.0.0.0', 3490)
+sock.bind(server_address)
+# Listen for incoming connections
+sock.listen(4)
+print('Esperando conexiones...')
+while True:
+    # Wait for a connection
+    client_socket, client_address = sock.accept()
+    # Inicia un nuevo hilo para manejar la conexión con el cliente
+    client_thread = threading.Thread(target=handle_client, args=(client_socket,))
+    client_thread.start()
